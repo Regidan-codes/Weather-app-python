5162504ab37d9cb7249059a744766514f85b9b7c
Regidan-codes <abhinadiger@gmail.com>
2023-11-28 12:25:15 +0530

    Added function and api key

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
+# Added title, slider, text input-box, sub-header
 st.title("Weather forecast for the next days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -9,5 +11,10 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky",))
 st.subheader(f"{option} for the next {days} day(s) in {place}")
 
-figure = px.line(x="Date", y="Temperature", labels={"x": "Date", "y": "Temperature (C)"})
+
+# Get date and temperature/sky from openweather api
+d, t = get_data(place, days, option)
+
+# Plot using plotly
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
